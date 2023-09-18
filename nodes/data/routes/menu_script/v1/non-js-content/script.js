b85a15bf99086d103d41afacd369cdbffc00d614
mrFavoslav <owner@favoslav.cz>
2023-09-18 09:50:49 +0000

    npm update

--- nodes/data/routes/menu_script/v1/non-js-content/script.js
+++ nodes/data/routes/menu_script/v1/non-js-content/script.js
@@ -1,13 +1,13 @@
-document.querySelector(".menu").addEventListener("click", ()=>{
-  document.querySelector(".nav-links").classList.remove("hidden"),
-  document.querySelector(".menu").classList.toggle("hidden")
-  document.querySelector(".menu-close").classList.remove("hidden")
-}
-);
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelector(".menu").addEventListener("click", () => {
+    document.querySelector(".nav-links").classList.remove("hidden");
+    document.querySelector(".menu").classList.toggle("hidden");
+    document.querySelector(".menu-close").classList.remove("hidden");
+  });
 
-document.querySelector(".menu-close").addEventListener("click", ()=>{
-  document.querySelector(".nav-links").classList.toggle("hidden"),
-  document.querySelector(".menu-close").classList.toggle("hidden")
-  document.querySelector(".menu").classList.remove("hidden")
-}
-);
+  document.querySelector(".menu-close").addEventListener("click", () => {
+    document.querySelector(".nav-links").classList.toggle("hidden");
+    document.querySelector(".menu-close").classList.toggle("hidden");
+    document.querySelector(".menu").classList.remove("hidden");
+  });
+});
