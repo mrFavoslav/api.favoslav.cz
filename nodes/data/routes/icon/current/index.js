4636fe8c6fb9ceaaea23ba19ff10db2388317ba8
mrFavoslav <owner@favoslav.cz>
2023-10-20 22:21:23 +0000

    Allot of non critical changes

--- nodes/data/routes/icon/current/index.js
+++ nodes/data/routes/icon/current/index.js
@@ -18,4 +18,10 @@ router.get('/', async ({ query }, res) => {
     res.sendFile("icon.webp", { root: "/home/api/nodes/data/routes/icon/current/non-js-content" });
   });
 
+router.get('/drift', async ({ query }, res) => {
+    const dir = __filename.replace('/home/api/nodes', '').replace('/routes', '').replace('index.js', '');
+    console.log(`[BOApi] Received GET request for ${dir}`);
+    res.sendFile("video.mp4", { root: "/home/api/nodes/data/routes/icon/current/non-js-content" });
+  });
+
 module.exports = router;
